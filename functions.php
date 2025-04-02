4c5b375d2159cdad4f240ba2d6b91062353f309c
HelenTkacheva <2371643@johnabbottcollege.net>
2025-04-02 12:20:56 -0400

    Final finished site

--- functions.php
+++ functions.php
@@ -5,21 +5,7 @@
         exit; 
         // Exit if accessed directly. 
         // Ex: Not being called up directly by index.php or some other theme PHP file
-    ?><!--  START WORDPRESS GET_HEADER CONTENT IN INDEX.PHP -->
-    <?php get_header(); ?>
-    <?php // this function loads the header.php file ?>
-
-
-    <!--  END WORDPRESS GET_HEADER CONTENT IN INDEX.PHP -->
-    <!-- STEP 2.3 ENDS HERE -->
-    <?php
- // Add this to all your php files for added security
-
- if (!defined('ABSPATH'))
-     exit; 
-     // Exit if accessed directly. 
-     // Ex: Not being called up directly by index.php or some other theme PHP file
- 
+    
  // FOR COLLEGE NETWORK SECURITY
  // FORCE AUTO-UPDATE EVERYTHING (WP CORE, PLUGINS AND THEMES)
  add_filter( 'allow_minor_auto_core_updates', '__return_true' );         // ENABLE MINOR UPDATES
